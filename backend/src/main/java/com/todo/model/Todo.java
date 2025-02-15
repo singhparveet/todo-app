841f813cf0a9fc1251f168a927639c645526c679
Parveet Singh <singh.parveet@gmail.com>
2025-02-16 02:10:09 +0530

    fix

--- backend/src/main/java/com/todo/model/Todo.java
+++ backend/src/main/java/com/todo/model/Todo.java
@@ -1,17 +1,59 @@
 package com.todo.model;
 
 import jakarta.persistence.*;
-import lombok.*;
 
 @Entity
-@Table(name = "todos")
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
 public class Todo {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String title;
     private boolean completed;
+
+    // Default Constructor
+    public Todo() {
+    }
+
+    // Parameterized Constructor
+    public Todo(Long id, String title, boolean completed) {
+        this.id = id;
+        this.title = title;
+        this.completed = completed;
+    }
+
+    // Getters and Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public boolean isCompleted() {
+        return completed;
+    }
+
+    public void setCompleted(boolean completed) {
+        this.completed = completed;
+    }
+
+    // toString Method
+    @Override
+    public String toString() {
+        return "Todo{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", completed=" + completed +
+                '}';
+    }
 }
